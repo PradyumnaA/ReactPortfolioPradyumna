848c2c0a8e7d8a4fc65f411f9151e2c2d1ab4675
PradyumnaAnilKumarKubear1998 <p_anilkumarkubear@u.pacific.edu>
2024-04-22 19:40:57 -0700

    personal profile modification navbar's logo done youtube 27:49 done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,40 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {styles} from "../styles.js";
+import {navLinks} from "../constants/index.js";
+import {logoColor,menu,close} from '../assets'
 const Navbar = () => {
-  return(
-      <>
-        Navbar
-      </>
-  )
+  const[active,setActive]=useState('""');
+    return(
+
+      <nav
+          className={`${styles.paddingX}
+          w-full flex items-center py-5
+          fixed top-0 z-20
+          bg-primary
+          `}>
+          <div className='w-full flex justify-between
+          items-center py-5 fixed top-0 z-20
+          bg-primary'>
+
+              <Link to="/"
+                    className='flex items-center gap-2'
+                    onClick={() => {
+                        setActive("");
+                        window.scrollTo(0, 0);
+                    }}
+
+              >
+                  <img src={logoColor} alt='logo'
+                       className='object-cover'
+                       width={150} height={150}
+                  />
+              </Link>
+
+{/*ok*/}
+          </div>
+
+      </nav>
+    )
 }
 export default Navbar
